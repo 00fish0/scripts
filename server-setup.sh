49c4837fb4356683fc1afc397cb5ad05aa3e2169
Tang Zihan <tzh2005t@163.com>
2024-06-22 22:06:11 +0800

    chore: extract variables

--- server-setup.sh
+++ server-setup.sh
@@ -2,77 +2,80 @@
 
 # Untested
 
-server="pi@10.250.116.205"
-network_account="2023311B21 tzh20050911"
+server=""
+network_account=""
 startup_sec="10s"
+pub_key=""
 
 CACHE_ROOT=/tmp/deploy_cache
 STAGE=-1
 TOT_STAGE=5
 
-function update_stage() {
-	STAGE=$(($STAGE + 1))
-	echo $STAGE >$CACHE_ROOT/stage
-}
 
-function print_stage() {
-  echo -e "\033[31m[STAGE $1] $2\033[0m"
-  update_stage
-}
 function error() {
-    echo -e "\033[31m[ERROR] $1\033[0m"
+    echo -e "\033[31m[ERROR] $1\033[0m" >&2
     exit 1
 }
 function log() {
   echo -e "\033[32m[LOG] $1\033[0m"
 }
+
+function print_stage() {
+  log "Stage $1: $2"
+  update_stage
+}
+function update_stage() {
+	STAGE=$(($STAGE + 1))
+	echo $STAGE >$CACHE_ROOT/stage
+}
+
 # cat ~/.ssh/id_rsa.pub | ssh $server "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 
 function stage_0() {
   print_stage 0 "ssh passwordless login"
 
-  cat << 'EOF' >> ~/.ssh/authorized_keys
-  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC25xRSeFNmFYr+EstSVErlPIQtT6SWn+YzUz/py70uVUVHKqXhBjuAttCqJ1SQXBHs84BamzRQQOL/Rc38slv0cO030++9Yw4QLbhRPxbAkJPIkrRisL9YX0gG+dvISXRPJy1u/FXZyJQv6TEf2ELiF7Xa8vcnSAhV4oGd/C/ePrpZYzFpX1+Vj0zl627OdFhBRv5TfpWcXRXmfvZ2OtOR/duxXNqavGrNKYt09qLIMAfkLPbVVWss7FIHOR+kcv/bzi49wGbbWhHw4xI3TsPwFG5reiYQVdODaACKi0TopW5BupGf6WJyX95eLZfCyzRD4COQ20p1jRVip4uSv47r77Br9IzrGmUSj5KI9xo9XhYGoVuvrjlpk1Wvp3qOTBzYgvZw/O/+aPgpuL6AQyIjEKdFGQ1jmPF3s9ki+J8Ux0Oppdtgd9ov14/gv9tFOR7VTtOuJIwAyGcm0yyS8C46oZwgXMatqsMNM84/T56Ka/H+mB5oqo5oXANZtyxjR0s= TZH@LAPTOP-S5UUUS6Q
+  cat << EOF >> ~/.ssh/authorized_keys
+$pub_key
 EOF
 }
 
 function stage_1() {
-  print_stage 1 "Setup School Network Login Script"
+#   print_stage 1 "Setup School Network Login Script"
 
-  mkdir -p ~/auto_login
-  wget -P ~/auto_login https://github.com/hstable/SRUN_LOGIN/releases/download/v1.1.1/SRUN_LOGIN_linux_arm -O ~/auto_login/SRUN_LOGIN
+#   mkdir -p ~/auto_login
+#   wget -P ~/auto_login https://github.com/hstable/SRUN_LOGIN/releases/download/v1.1.1/SRUN_LOGIN_linux_arm -O ~/auto_login/SRUN_LOGIN
 
-  cd ~/auto_login || error "cd failed"
-  cat << 'EOF' > login.sh # here-document
-  echo "[LOG] $(date '+%Y-%m-%d %H:%M:%S')" >> ~/auto_login/login.log
-  ~/auto_login/SRUN_LOGIN $network_account >> ~/auto_login/login.log 2>&1
-EOF
+#   cd ~/auto_login || error "cd failed"
+#   cat << 'EOF' > login.sh # here-document
+# echo "[LOG] $(date '+%Y-%m-%d %H:%M:%S')" >> ~/auto_login/login.log
+# ~/auto_login/SRUN_LOGIN $network_account >> ~/auto_login/login.log 2>&1
+# EOF
 }
 
 function stage_2() {
-  print_stage 2 "Create system service & timer"
+#   print_stage 2 "Create system service & timer"
 
-  cat << 'EOF' > /etc/systemd/system/srun_login.service
-  [Unit]
-  Description=login school-net
+#   cat << 'EOF' > /etc/systemd/system/srun_login.service
+# [Unit]
+# Description=login school-net
 
-  [Service]
-  ExecStart=sudo /bin/bash ~/auto_login/login.sh
-EOF
+# [Service]
+# ExecStart=sudo /bin/bash ~/auto_login/login.sh
+# EOF
 
-  cat << 'EOF' > /etc/systemd/system/srun_login.timer  # if login fails with network setup error, increase OnStartupSec below.
-  [Unit]
-  Description=Runs login school-net script timer
+#   cat << EOF > /etc/systemd/system/srun_login.timer  # if login fails with network setup error, increase OnStartupSec below.
+# [Unit]
+# Description=Runs login school-net script timer
 
-  [Timer]
-  OnStartupSec=$startup_sec
-  Unit=srun_login.service
+# [Timer]
+# OnStartupSec=$startup_sec
+# Unit=srun_login.service
 
-  [Install]
-  WantedBy=multi-user.target
-EOF
+# [Install]
+# WantedBy=multi-user.target
+# EOF
 
-  sudo systemctl enable srun_login.timer
+#   sudo systemctl enable srun_login.timer
 }
 
 function stage_3() {
@@ -81,42 +84,42 @@ function stage_3() {
   if grep -q 'ubuntu' /etc/os-release; then
     log "Ubuntu detected."
     cat <<'EOF' > /etc/apt/sources.list
-    # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-    deb https://mirrors.osa.moe/ubuntu/ jammy main restricted universe multiverse
-    # deb-src https://mirrors.osa.moe/ubuntu/ jammy main restricted universe multiverse
-    deb https://mirrors.osa.moe/ubuntu/ jammy-updates main restricted universe multiverse
-    # deb-src https://mirrors.osa.moe/ubuntu/ jammy-updates main restricted universe multiverse
-    deb https://mirrors.osa.moe/ubuntu/ jammy-backports main restricted universe multiverse
-    # deb-src https://mirrors.osa.moe/ubuntu/ jammy-backports main restricted universe multiverse
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.osa.moe/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.osa.moe/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.osa.moe/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.osa.moe/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.osa.moe/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.osa.moe/ubuntu/ jammy-backports main restricted universe multiverse
 
-    # deb https://mirrors.osa.moe/ubuntu/ jammy-security main restricted universe multiverse
-    # # deb-src https://mirrors.osa.moe/ubuntu/ jammy-security main restricted universe multiverse
+# deb https://mirrors.osa.moe/ubuntu/ jammy-security main restricted universe multiverse
+# # deb-src https://mirrors.osa.moe/ubuntu/ jammy-security main restricted universe multiverse
 
-    deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-    # deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
 
-    # 预发布软件源，不建议启用
-    # deb https://mirrors.osa.moe/ubuntu/ jammy-proposed main restricted universe multiverse
-    # # deb-src https://mirrors.osa.moe/ubuntu/ jammy-proposed main restricted universe multiverse
+# 预发布软件源，不建议启用
+# deb https://mirrors.osa.moe/ubuntu/ jammy-proposed main restricted universe multiverse
+# # deb-src https://mirrors.osa.moe/ubuntu/ jammy-proposed main restricted universe multiverse
 EOF
   elif grep -q 'debian' /etc/os-release; then
     log "Debian detected."
     cat <<'EOF' > /etc/apt/sources.list
-    # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-    deb https://mirrors.osa.moe/debian/ bookworm main contrib non-free non-free-firmware
-    # deb-src https://mirrors.osa.moe/debian/ bookworm main contrib non-free non-free-firmware
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.osa.moe/debian/ bookworm main contrib non-free non-free-firmware
+# deb-src https://mirrors.osa.moe/debian/ bookworm main contrib non-free non-free-firmware
 
-    deb https://mirrors.osa.moe/debian/ bookworm-updates main contrib non-free non-free-firmware
-    # deb-src https://mirrors.osa.moe/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://mirrors.osa.moe/debian/ bookworm-updates main contrib non-free non-free-firmware
+# deb-src https://mirrors.osa.moe/debian/ bookworm-updates main contrib non-free non-free-firmware
 
-    deb https://mirrors.osa.moe/debian/ bookworm-backports main contrib non-free non-free-firmware
-    # deb-src https://mirrors.osa.moe/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb https://mirrors.osa.moe/debian/ bookworm-backports main contrib non-free non-free-firmware
+# deb-src https://mirrors.osa.moe/debian/ bookworm-backports main contrib non-free non-free-firmware
 
-    # deb https://mirrors.osa.moe/debian-security bookworm-security main contrib non-free non-free-firmware
-    # # deb-src https://mirrors.osa.moe/debian-security bookworm-security main contrib non-free non-free-firmware
+# deb https://mirrors.osa.moe/debian-security bookworm-security main contrib non-free non-free-firmware
+# # deb-src https://mirrors.osa.moe/debian-security bookworm-security main contrib non-free non-free-firmware
 
-    deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
-    # deb-src https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+# deb-src https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
 EOF
   else
     error "Unsupported OS."
@@ -132,11 +135,106 @@ function stage_4() {
 }
 
 function stage_5() {
-  print_stage 5 "Dotfiles"
+  print_stage 5 "Install OhMyZsh"
 
-  cd ~/auto_login || error "cd failed"
-  git clone -b server https://github.com/00fish0/dotfiles.git
-  ./dotfiles/setup.sh
+  sudo apt install zsh -y
+  sh -c "$(wget -O- https://install.ohmyz.sh/)"
+
+  log "Installing plugin autojump..."
+
+  sudo apt install autojump
+  if [[ -s /usr/share/autojump/autojump.sh ]]; then
+    echo '# oh-my-zsh setup' >> ~/.zshrc
+    echo '. /usr/share/autojump/autojump.sh' >> ~/.zshrc
+  else
+    error "autojump not found, check platform compatibility."
+  fi
+}
+
+function stage_6() {
+  print_stage 6 "Dotfiles"
+
+  rm {~/.vimrc,~/.gitconfig,~/.tmux.conf}
+
+  # cd ~/auto_login || error "cd failed"
+  # git clone -b server https://github.com/00fish0/dotfiles.git
+  # ./dotfiles/setup.sh
+  cat << 'EOF' > ~/.tmux.conf
+set -g prefix C-a
+ 
+unbind C-b
+ 
+bind C-a send-prefix
+EOF
+
+  cat << 'EOF' > ~/.vimrc
+" All system-wide defaults are set in $VIMRUNTIME/debian.vim and sourced by
+" the call to :runtime you can find below.  If you wish to change any of those
+" settings, you should do it in this file (/etc/vim/vimrc), since debian.vim
+" will be overwritten everytime an upgrade of the vim packages is performed.
+" It is recommended to make changes after sourcing debian.vim since it alters
+" the value of the 'compatible' option.
+
+runtime! debian.vim
+
+" Vim will load $VIMRUNTIME/defaults.vim if the user does not have a vimrc.
+" This happens after /etc/vim/vimrc(.local) are loaded, so it will override
+" any settings in these files.
+" If you don't want that to happen, uncomment the below line to prevent
+" defaults.vim from being loaded.
+" let g:skip_defaults_vim = 1
+
+" Uncomment the next line to make Vim more Vi-compatible
+" NOTE: debian.vim sets 'nocompatible'.  Setting 'compatible' changes numerous
+" options, so any other options should be set AFTER setting 'compatible'.
+"set compatible
+
+" Vim5 and later versions support syntax highlighting. Uncommenting the next
+" line enables syntax highlighting by default.
+if has("syntax")
+  syntax on
+endif
+
+" If using a dark background within the editing area and syntax highlighting
+" turn on this option as well
+"set background=dark
+
+" Uncomment the following to have Vim jump to the last position when
+" reopening a file
+"au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+
+" Uncomment the following to have Vim load indentation rules and plugins
+" according to the detected filetype.
+filetype plugin indent on
+
+" The following are commented out as they cause vim to behave a lot
+" differently from regular Vi. They are highly recommended though.
+"set showcmd		" Show (partial) command in status line.
+set showmatch		" Show matching brackets.
+set ignorecase		" Do case insensitive matching
+set smartcase		" Do smart case matching
+set incsearch		" Incremental search
+"set autowrite		" Automatically save before commands like :next and :make
+set hidden		" Hide buffers when they are abandoned
+"set mouse=a		" Enable mouse usage (all modes)
+
+" Source a global configuration file if available
+if filereadable("/etc/vim/vimrc.local")
+  source /etc/vim/vimrc.local
+endif
+EOF
+
+  cat << 'EOF' > ~/.gitconfig
+[user]
+	email = tzh2005t@163.com
+	name = tzh
+
+[alias]
+lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all
+lg = !"git lg1"
+EOF
+
 }
 
 function branch_continue() {
@@ -154,6 +252,12 @@ function branch_continue() {
 }
 
 function main() {
+
+  if [ "$(id -u)" != "0" ]; then
+    error "This script must be run as root"
+    return 1
+  fi
+
   if [ $# -gt 1 ]; then
     error "0/1 argument expected.\nUsage: $0 [clear|all] ..."
     return 1
